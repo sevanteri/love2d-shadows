b22a9ed308fbcb14ae0ba38dfbd2c1014494ca49
Pete Sevander <pete@sevander.com>
2015-08-30 13:57:48 +0300

    More lights, circle grid instead of a few boxes.

--- main.lua
+++ main.lua
@@ -4,11 +4,10 @@ local lightIndex = 1
 local pressing = false
 local lights = {
     light(256, 256, 256, {255,255,255}),
-    light(400, 300, 256, {255,0,0,150}),
-    light(200, 256, 215, {0,0,255,200})
-    --light(200, 256, 256, {0,255,0,200}),
-    --light(200, 256, 256, {10,10,10,200}),
-    --light(200, 256, 256, {0,255,255,200})
+    light(200, 256, 215, {0,0,255,200}),
+    light(0, 0, 400, {255,255,0, 200}),
+    light(800, 0, 500, {200,0,200,200}),
+    light(0, 600, 300, {0, 200, 200, 200})
 }
 
 local g = love.graphics
@@ -17,14 +16,28 @@ function love.load()
     love.window.setMode(800, 600)
     Wwidth, Wheight = love.window.getDimensions()
     Xcenter, Ycenter = Wwidth/2, Wheight/2
-    g.setBackgroundColor(200, 200, 200)
+    g.setBackgroundColor(100, 100, 100)
 
     objectCanvas = g.newCanvas(Wwidth, Wheight)
     objectCanvas:renderTo(function()
         g.setColor(50, 50, 50, 255)
-        g.rectangle('fill', Xcenter - 100, Ycenter - 100, 50, 50)
-        g.rectangle('fill', Xcenter + 50, Ycenter + 50, 50, 50)
-        g.rectangle('fill', Xcenter - 50, Ycenter + 50, 50, 50)
+        local w = 8 h = 8
+        local pad = 40 s = 10
+        local circlesSize = function(x) return (1 + x) * pad; end
+        local xmargin = objectCanvas:getWidth()/2 - circlesSize(w)/2
+        local ymargin = objectCanvas:getHeight()/2 - circlesSize(h)/2
+        for i=1, w do
+            for j=1, h do
+                g.circle('fill',
+                         xmargin + i * pad,
+                         ymargin + j * pad,
+                         s)
+            end
+        end
+
+        --g.rectangle('fill', Xcenter - 100, Ycenter - 100, 50, 50)
+        --g.rectangle('fill', Xcenter + 50, Ycenter + 50, 50, 50)
+        --g.rectangle('fill', Xcenter - 50, Ycenter + 50, 50, 50)
     end)
 
 end
